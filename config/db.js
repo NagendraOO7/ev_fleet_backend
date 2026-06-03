845823e38bb6f12a41370a26ae6846dac36736ee
Nagendra-in-hackingly <nagendra@hackingly.in>
2026-06-03 09:46:47 +0530

    db config

--- config/db.js
+++ config/db.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
 module.exports = {
     connectDB: () => mongoose.connect(
-        process.env.MONGO_URI || 'mongodb+srv://nagendrahada1609_db_user:vIAxCZSntVC0Nmh9@e3electric.ai9cg30.mongodb.net/e3electric'
+        process.env.MONGO_URI || 'mongodb+srv://nagendrahada1609_db_user:vIAxCZSntVC0Nmh9@e3electric.ai9cg30.mongodb.net/e3electric',{
+            tls: true,
+        tlsAllowInvalidCertificates: false,
+        serverSelectionTimeoutMS: 5000,
+        }
         //                                                                                                               
     ).then(() => {
         // console.log('DB name:', mongoose.connection.db.databaseName);
