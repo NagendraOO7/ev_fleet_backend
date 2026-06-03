37a0830d9437dbd8e78acf3cf9e7b96b6eba5296
Nagendra-in-hackingly <nagendra@hackingly.in>
2026-06-03 11:33:57 +0530

    fix

--- routes/vehicles.js
+++ routes/vehicles.js
@@ -10,7 +10,7 @@ router.get('/', async (req, res) => {
         const page = Number(req.query.page) || 1;
 
         // Prevent huge requests
-        const limit = Math.min(Number(req.query.limit) || 20, 100);
+        const limit = req.query.limit;
 
         const match = search
             ? {
